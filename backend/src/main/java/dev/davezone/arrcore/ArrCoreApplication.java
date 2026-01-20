320772e383989d2f3ca97a8a54303dfac3fe8e43
Davessonn <babaidavid96@gmail.com>
2026-01-20 17:09:45 +0100

    init

---
+++ backend/src/main/java/dev/davezone/arrcore/ArrCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.davezone.arrcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrCoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/davezone/arrcore/ArrCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.davezone.arrcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
