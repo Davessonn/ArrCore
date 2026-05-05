8a402a88ba55354c8ea0ddabee9dede867d4dbdb
dbabai <dbabai@precognox.com>
2026-05-05 15:15:40 +0200

    FE - Remove HashRouter from App component and update package dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Index from './pages/Index'
 import Portainer from './pages/Portainer'
 import Sonarr from './pages/Sonarr'
@@ -6,14 +6,12 @@ import './App.css'
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/portainer" element={<Portainer />} />
-        <Route path="/sonarr" element={<Sonarr />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/portainer" element={<Portainer />} />
+      <Route path="/sonarr" element={<Sonarr />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
 
