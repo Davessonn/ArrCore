e31e45825efeddf30ee8625ad4cd23ab81047441
Davessonn <babaidavid96@gmail.com>
2026-05-23 20:47:01 +0200

    BE - Update PortainerService to use API key for authentication and remove JWT logic

--- backend/src/main/java/dev/davezone/arrcore/service/PortainerService.java
+++ backend/src/main/java/dev/davezone/arrcore/service/PortainerService.java
@@ -1,22 +1,17 @@
 package dev.davezone.arrcore.service;
 
-import dev.davezone.arrcore.auth.AuthRequest;
-import dev.davezone.arrcore.auth.AuthResponse;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Service
 public class PortainerService {
 
-    private static final String GET_JWT_API_PATH = "api/auth";
     private static final String GET_CONTAINERS_API_PATH = "api/endpoints/3/docker/containers/json";
 
     private final String portainerUrl;
-    private final String portainerUsername;
-    private final String portainerPassword;
+    private final String portainerApiKey;
 
     private final WebClient webClient;
 
@@ -24,32 +19,19 @@ public class PortainerService {
         Dotenv dotenv = Dotenv.load();
 
         this.portainerUrl = dotenv.get("PORTAINER_URL");
-        this.portainerUsername = dotenv.get("PORTAINER_USERNAME");
-        this.portainerPassword = dotenv.get("PORTAINER_PASSWORD");
+        this.portainerApiKey = dotenv.get("PORTAINER_API_KEY");
         this.webClient = webClient;
 
+        if (this.portainerUrl == null || this.portainerApiKey == null) {
+            throw new IllegalStateException("Missing required environment variables: PORTAINER_URL and PORTAINER_API_KEY must be set.");
+        }
     }
 
     public Flux<ContainerResponse> getContainers() {
-        return authenticate()
-                .flatMapMany(this::fetchContainers);
-    }
-
-    private Mono<String> authenticate() {
-        return webClient.post()
-                .uri(portainerUrl + GET_JWT_API_PATH)
-                .bodyValue(new AuthRequest(portainerUsername, portainerPassword))
-                .retrieve()
-                .bodyToMono(AuthResponse.class)
-                .map(AuthResponse::getJwt);
-    }
-
-    private Flux<ContainerResponse> fetchContainers(String token) {
         return webClient.get()
                 .uri(portainerUrl + GET_CONTAINERS_API_PATH + "?all=true")
-                .header("Authorization", "Bearer " + token)
+                .header("X-API-Key", portainerApiKey)
                 .retrieve()
                 .bodyToFlux(ContainerResponse.class);
     }
-
 }
